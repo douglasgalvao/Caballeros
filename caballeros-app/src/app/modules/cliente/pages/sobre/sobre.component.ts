35b6e610a00bff9352963a02f29547e85491ed3b
douglasgalvao <douglasgmachado98@gmail.com>
2022-11-09 19:56:27 -0300

    Fix deleting token when the request get error

--- caballeros-app/src/app/modules/cliente/pages/sobre/sobre.component.ts
+++ caballeros-app/src/app/modules/cliente/pages/sobre/sobre.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 import { AuthService } from 'src/app/modules/auth/auth.service';
 
@@ -12,12 +13,16 @@ export class SobreComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
+    private cookieService: CookieService,
     private router: Router
   ) { }
 
 
 
   ngOnInit(): void {
-    this.authService.verifyIsLogged().subscribe((e: any) => {},(error=>{this.router.navigate(["/auth/login"])}));
+    this.authService.verifyIsLogged().subscribe((e: any) => { }, (error => {
+      this.cookieService.delete('token');
+      this.router.navigate(["/auth/login"]);
+    }));
   }
 }
